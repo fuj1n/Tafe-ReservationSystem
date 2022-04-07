0f1cc4060acf6c5760c2a02bfe3e2314e224f660
mc305 <matthew.chen01@gmail.com>
2022-04-07 17:22:14 +1000

    Added Details Controller and Member Area

---
+++ ReservationSystem-Server/ReservationSystem-Server/Areas/Member/Controllers/DetailsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationSystem_Server.Areas.Member.Controllers
+{
+    public class DetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
