135e00101172b49832ae616f2a6892eab3623d0e
mc305 <matthew.chen01@gmail.com>
2022-04-23 14:52:06 +1000

    Added Reservation Controller

---
+++ ReservationSystem-Server/ReservationSystem-Server/Controllers/ReservationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReservationSystem_Server.Data;
+
+namespace ReservationSystem_Server.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private ApplicationDbContext _context;
+        public ReservationController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Sittings()
+        {
+            var sittings = await _context.Sittings.ToListAsync();
+
+            return View(sittings);
+        }
+
+        public async Task<IActionResult> Create(int sittingId)
+        {
+            var sitting = await _context.Sittings.Include(s=>s.SittingType).FirstAsync(s=>s.Id == sittingId);
+
+            var vm = new Models.Reservation.CreateVM 
+            {
+                SittingId = sitting.Id,
+                SittingStartTime = sitting.StartTime,
+                SittingEndTime = sitting.EndTime,
+                SittingType = sitting.SittingType.Description
+
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ ReservationSystem-Server/ReservationSystem-Server/Models/Reservation/CreateVM.cs
@@ -0,0 +1,22 @@
+﻿using ReservationSystem_Server.Data;
+
+namespace ReservationSystem_Server.Models.Reservation
+{
+    public class CreateVM
+    {
+        //start time, durationno. of guests, Notes/Add req
+        public int SittingId { get; set; }
+        public DateTime SittingStartTime { get; set; }   
+        public DateTime SittingEndTime { get; set; }   
+
+        public string SittingType { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get ; set; }
+
+        public int NoOfGuests { get; set; }
+
+        public string Notes { get; set; }
+
+    }
+}
