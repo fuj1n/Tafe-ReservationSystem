31cb58a7c730d36cbd60940334388e7452f075bc
Arthur (fuj1n) Uzulin <fuj1nman@hotmail.com>
2022-04-23 14:49:27 +1000

    Added admin area

---
+++ ReservationSystem-Server/ReservationSystem-Server/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationSystem_Server.Areas.Admin.Controllers;
+
+public class HomeController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
