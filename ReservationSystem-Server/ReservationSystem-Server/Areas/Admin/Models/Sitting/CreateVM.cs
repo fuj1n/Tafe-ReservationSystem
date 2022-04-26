4fa864dd0a6d23ac2a994cd8fe0d4a150f5fcbb1
WencongZuo <zuowencong@gmail.com>
2022-04-26 13:03:57 +1000

    Admin Sitting Controller

---
+++ ReservationSystem-Server/ReservationSystem-Server/Areas/Admin/Models/Sitting/CreateVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ReservationSystem_Server.Areas.Admin.Models.Sitting
+{
+    public class CreateVM
+    {
+        [ValidateNever]
+        public DateTime StartTime { get; set; }
+        [ValidateNever]
+        public DateTime EndTime { get; set; }
+        public int Capacity { get; set; }
+        public int SittingType { get; set; }
+        public SelectList? SittingTypes { get; set; }
+    }
+}
---
+++ ReservationSystem-Server/ReservationSystem-Server/Areas/Admin/Models/Sitting/EditVM.cs
@@ -0,0 +1,7 @@
+﻿namespace ReservationSystem_Server.Areas.Admin.Models.Sitting
+{
+    public class EditVM: CreateVM
+    {
+        public int Id { get; set; }
+    }
+}
