47874453b2c9ec8949a69f7eef7b7b6502a7ea3f
WencongZuo <zuowencong@gmail.com>
2022-04-28 15:59:52 +1000

    Add Validations and UI features

--- ReservationSystem-Server/ReservationSystem-Server/Areas/Admin/Models/Sitting/CreateVM.cs
+++ ReservationSystem-Server/ReservationSystem-Server/Areas/Admin/Models/Sitting/CreateVM.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace ReservationSystem_Server.Areas.Admin.Models.Sitting
 {
     public class CreateVM
     {
-        [ValidateNever]
+        //[ValidateNever]
         public DateTime StartTime { get; set; }
-        [ValidateNever]
+        //[ValidateNever]
         public DateTime EndTime { get; set; }
+        [Range(0, 10000)]
         public int Capacity { get; set; }
+        [Required(ErrorMessage = "Please select a sitting type")]
         public int SittingType { get; set; }
         public SelectList? SittingTypes { get; set; }
     }
