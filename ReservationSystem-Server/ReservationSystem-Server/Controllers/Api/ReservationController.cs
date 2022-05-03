aed57f961932f20c617bbeda3fc4c2b0eac101bb
mc305 <matthew.chen01@gmail.com>
2022-05-03 23:56:24 +1000

    Added Reservation API Controller and Sitting VM

---
+++ ReservationSystem-Server/ReservationSystem-Server/Controllers/Api/ReservationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReservationSystem_Server.Data;
+using ReservationSystem_Server.Models.Reservation.Api;
+using ReservationSystem_Server.Services;
+
+namespace ReservationSystem_Server.Controllers.Api
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class ReservationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ReservationUtility _utility;
+        private readonly CustomerManager _customerManager;
+        public ReservationController(ApplicationDbContext context, ReservationUtility utility, CustomerManager customerManager)
+        {
+            _context = context;
+            _utility = utility;
+            _customerManager = customerManager;
+        }
+
+        [HttpGet("sittings")]
+        public async Task<IActionResult> Sittings()
+        {
+            var sittings = await _context.Sittings.Include(s => s.SittingType).Where(s => s.IsClosed == false && s.StartTime > DateTime.Now)
+                            .Select(s=>new SittingVM {
+                                Id = s.Id,
+                                StartTime = s.StartTime,
+                                EndTime = s.EndTime,
+                                IsClosed = s.IsClosed,
+                                Capacity = s.Capacity,
+                                SittingType = s.SittingType.Description
+                            }).ToListAsync();
+
+
+            return Ok(sittings);
+        }
+    }
+}
