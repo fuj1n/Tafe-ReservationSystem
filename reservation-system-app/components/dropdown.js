d6ede7b4de38bb77292c1b7224a67157f66e84f3
Arthur (fuj1n) Uzulin <fuj1nman@hotmail.com>
2022-05-07 22:19:54 +1000

    Untested fix for dropdown on iOS and fixed login screen

--- reservation-system-app/components/dropdown.js
+++ reservation-system-app/components/dropdown.js
@@ -1,8 +1,43 @@
 import style from "./style";
-import {Text, View} from "react-native";
+import {Platform, Text, View} from "react-native";
 import {Picker} from "@react-native-picker/picker";
+import RNPickerSelect from 'react-native-picker-select';
 
+
+function DropdownIOS(props) {
+    const {label, items, children, style: userStyle} = props;
+
+    let itemsList;
+    if(children) {
+        itemsList = children.map(item => {
+            return {label: item.props.label, value: item.props.value};
+        });
+    } else {
+        itemsList = items;
+    }
+
+    return (
+        <View style={[style.inputContainer, userStyle]}>
+            {label && <Text style={style.inputLabel}>{label}</Text>}
+            <View style={style.dropdownInput}>
+                <RNPickerSelect {...props} mode="dropdown"
+                                items={itemsList} placeholder={{}} value={props.selectedValue}
+                                style={{inputWeb: style.dropdownWebInputStyle}}/>
+            </View>
+        </View>
+    );
+}
+
+/**
+ * @param props {{label : string, items : [{label : string, value, color?: ColorValue}], children, style,
+ * onValueChange : function(string), selectedValue, enabled : boolean, forceIosMode : boolean}}
+ * @remarks props.forceIosMode is used to force iOS mode even if the platform is not iOS, and is intended for testing purposes only
+ */
 export default function Dropdown(props) {
+    if(Platform.OS === "ios" || props.forceIosMode) {
+        return <DropdownIOS {...props} />;
+    }
+
     const {label, items, children, style: userStyle} = props;
 
     let itemsDom;
