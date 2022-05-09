1292ff44a84b18705d865c59fadabcb2f115c86a
Arthur (fuj1n) Uzulin <fuj1nman@hotmail.com>
2022-05-09 18:21:56 +1000

    Implemented radio buttons

--- reservation-system-app/pages/testPalette/SecondScreen.js
+++ reservation-system-app/pages/testPalette/SecondScreen.js
@@ -1,6 +1,8 @@
 import {View} from "react-native";
 import styles from "../styles";
 import {Button, StyledText} from "../../components";
+import Radio, {RadioGroup} from "../../components/radio";
+import Toggle from "../../components/toggle";
 
 export default function SecondScreen(props) {
     const {navigation} = props;
@@ -9,6 +11,28 @@ export default function SecondScreen(props) {
         <View style={styles.container}>
             <StyledText style={styles.containerItem} variant="danger">Second screen</StyledText>
             <Button style={styles.containerItem} variant="primary" onPress={() => navigation.goBack()}>Go back</Button>
+
+            <RadioGroup style={styles.containerItem} label="Vertical Radio Group:" direction="column">
+                <Radio label="Option One" value={1}/>
+                <Radio label="Option Two" value={2}/>
+            </RadioGroup>
+
+            <RadioGroup style={styles.containerItem} label="Horizontal Radio Group:" direction="row">
+                <Radio label="Option One" value={1}/>
+                <Radio label="Option Two" value={2}/>
+            </RadioGroup>
+
+            <RadioGroup style={styles.containerItem} label="Vertical Button Radio Group:" direction="column" mode="button">
+                <Radio label="Option One" value={1}/>
+                <Radio label="Option Two" value={2}/>
+            </RadioGroup>
+
+            <RadioGroup style={styles.containerItem} label="Horizontal Button Radio Group:" direction="row" mode="button">
+                <Radio label="Option One" value={1}/>
+                <Radio label="Option Two" value={2}/>
+            </RadioGroup>
+
+            <Toggle style={styles.containerItem} label="Checkbox Toggle" mode="checkbox" />
         </View>
     )
 }
