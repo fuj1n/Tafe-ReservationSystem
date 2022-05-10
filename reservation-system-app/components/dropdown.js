ebabe08e0908a828010ecfae79ccc4d3e64f138a
Arthur Uzulin <fuj1nman@hotmail.com>
2022-05-10 13:05:00 +1000

    Added dropdown chevron on iOS

--- reservation-system-app/components/dropdown.js
+++ reservation-system-app/components/dropdown.js
@@ -1,10 +1,11 @@
 import style from "./style";
 import {Platform, Text, View} from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 // Has to be done conditionally, otherwise crashes on mobile due to conflict between Picker and PickerSelect
 let Picker;
 let DropdownIOS = (_) => (<></>);
-if(Platform.OS === "ios") {
+if (Platform.OS === "ios") {
     let RNPickerSelect;
     import("react-native-picker-select").then(module => {
         RNPickerSelect = module.default;
@@ -14,25 +15,28 @@ if(Platform.OS === "ios") {
         const {label, items, children, style: userStyle} = props;
 
         let itemsList;
-        if(children) {
+        if (children) {
             itemsList = children.map(item => {
                 return {label: item.props.label, value: item.props.value};
             });
         } else {
             itemsList = items;
         }
-
         return (
             <View style={[style.inputContainer, userStyle]}>
                 {label && <Text style={style.inputLabel}>{label}</Text>}
-                <View style={style.dropdownInput}>
-                    <RNPickerSelect {...props} mode="dropdown"
+                <View style={[style.dropdownInput, style.dropdownIosInput]}>
+                    <RNPickerSelect {...props}
                                     items={itemsList} placeholder={{}} value={props.selectedValue}
-                                    style={{inputWeb: style.dropdownWebInputStyle}}/>
+                                    style={{
+                                        inputWeb: style.dropdownWebInputStyle,
+                                        viewContainer: style.dropdownIosViewContainer
+                                    }}/>
+                    <MaterialIcons name="arrow-drop-down" size={24}/>
                 </View>
             </View>
         );
-    }
+    };
 } else {
     import("@react-native-picker/picker").then(module => {
         Picker = module.Picker;
@@ -52,18 +56,18 @@ export function DropdownItem(props) {
  * onValueChange : function(string), selectedValue, enabled : boolean}}
  */
 export default function Dropdown(props) {
-    if(Platform.OS === "ios") {
+    if (Platform.OS === "ios") {
         return <DropdownIOS {...props} />;
     }
 
     const {label, items, children, style: userStyle} = props;
 
     let itemsDom;
-    if(children) {
+    if (children) {
         itemsDom = children;
     } else {
         itemsDom = items.map((item, index) => {
-            return <Picker.Item key={index} {...item}/>
+            return <Picker.Item key={index} {...item}/>;
         });
     }
 
