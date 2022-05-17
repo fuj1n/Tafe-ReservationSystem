7db2621821d22bca999ab7c3657b86105d658131
Arthur (fuj1n) Uzulin <fuj1nman@hotmail.com>
2022-05-17 22:30:17 +1000

    Complete UI tests for create sitting use case

---
+++ ReservationSystem-Server/ReservationSystem-Server.UiTests/Util/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace ReservationSystem_Server.UiTests.Util;
+
+public static class Config
+{
+    public const string BaseUrl = "https://localhost:7264";
+    public const string FailureScreenshotPath = "test_failures";
+}
---
+++ ReservationSystem-Server/ReservationSystem-Server.UiTests/Util/TestUtility.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using OpenQA.Selenium;
+
+namespace ReservationSystem_Server.UiTests.Util;
+
+[PublicAPI]
+public static class TestUtility
+{
+    private static readonly Dictionary<DefaultAccount, (string, string)> Accounts = new()
+    {
+        { DefaultAccount.Admin, ("a@e.com", "Abc123!@#") },
+        { DefaultAccount.Manager, ("m@e.com", "Abc123!@#") },
+        { DefaultAccount.Employee, ("e@e.com", "Abc123!@#") },
+        { DefaultAccount.Customer, ("u@e.com", "Abc123!@#") }
+    };
+
+    /**
+     * Utility method that handles authentication for the test.
+     */
+    public static void Login(IWebDriver driver, string username, string password)
+    {
+        driver.Navigate().GoToUrl(Config.BaseUrl + "/Identity/Account/Login");
+        
+        driver.FindElement(By.Id("Input_Email")).SendKeys(username);
+        driver.FindElement(By.Id("Input_Password")).SendKeys(password);
+        driver.FindElement(By.Id("login-submit")).Click();
+    }
+    
+    /**
+     * Utility method that handles authentication as specific default account for the test.
+     */
+    public static void LoginAs(IWebDriver driver, DefaultAccount account)
+    {
+        (string username, string password) = Accounts[account];
+        Login(driver, username, password);
+    }
+    
+    public static void SendDateTime(IWebElement element, DateTime dateTime)
+    {
+        SendDate(element, DateOnly.FromDateTime(dateTime));
+        element.SendKeys(Keys.ArrowRight);
+        SendTime(element, TimeOnly.FromDateTime(dateTime));
+    }
+    
+    public static void SendDate(IWebElement element, DateOnly date)
+    {
+        element.SendKeys(date.ToString("ddMMyyyy"));
+    }
+    
+    public static void SendTime(IWebElement element, TimeOnly time)
+    {
+        element.SendKeys(time.ToString("hhmmtt"));
+    }
+    
+    public static bool IsFullyLoaded(IWebDriver driver)
+    {
+        return ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete");
+    }
+    
+    public enum DefaultAccount
+    {
+        Admin,
+        Manager,
+        Employee,
+        Customer
+    }
+}
