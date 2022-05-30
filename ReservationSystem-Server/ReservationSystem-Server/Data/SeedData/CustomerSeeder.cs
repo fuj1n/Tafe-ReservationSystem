6ad696ec631029fb7634dc33791ac117fe6266e8
Arthur Uzulin <fuj1nman@hotmail.com>
2022-05-30 10:59:04 +1000

    Added ability for member to view their reservations

---
+++ ReservationSystem-Server/ReservationSystem-Server/Data/SeedData/CustomerSeeder.cs
@@ -0,0 +1,20 @@
+﻿using ReservationSystem_Server.Helper.DataSeed;
+
+namespace ReservationSystem_Server.Data.SeedData;
+
+public static class CustomerSeeder
+{
+    [DataSeeder]
+    private static void SeedData(List<Customer> customers)
+    {
+        customers.Add(new Customer
+        {
+            Id = 1,
+            UserId = "de52760d-3485-4a2a-b892-4e389dfe44b8",
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "U@E.COM",
+            PhoneNumber = "0412345678"
+        });
+    }
+}
