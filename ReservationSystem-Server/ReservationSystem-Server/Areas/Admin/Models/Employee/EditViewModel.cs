e2dedadf321cf5c46f3f22f87fe8dc2c0fbaedd6
Arthur (fuj1n) Uzulin <fuj1nman@hotmail.com>
2022-06-13 17:00:44 +1000

    Fixed edit employee not working due to non-existing password field validation

--- ReservationSystem-Server/ReservationSystem-Server/Areas/Admin/Models/Employee/EditViewModel.cs
+++ ReservationSystem-Server/ReservationSystem-Server/Areas/Admin/Models/Employee/EditViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace ReservationSystem_Server.Areas.Admin.Models.Employee;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class EditViewModel : CreateViewModel
+namespace ReservationSystem_Server.Areas.Admin.Models.Employee;
+
+public class EditViewModel
 {
     public string Id { get; set; } = null!;
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+    [Phone]
+    public string? PhoneNumber { get; set; }
 }
